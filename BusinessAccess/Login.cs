1f76e2783d5fef4e726fede256b5743edc8d2327
hoang02021993-lesp <hoang02021993@gmail.com>
2020-04-27 22:10:30 -0700

    gl04-lesp04

---
+++ BusinessAccess/Login.cs
@@ -0,0 +1,59 @@
+﻿using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BusinessAccess
+{
+    class Login
+    {
+        void DangNhapGoLike(string ID, string Pass, ChromeDriver driver)
+        {
+            try
+            {
+                var _id = driver.FindElementByXPath("//*[@id=\"app\"]/div/div/div/form/div[1]/input");
+                _id.SendKeys(ID);
+                var _pass = driver.FindElementByXPath("//*[@id=\"app\"]/div/div/div/form/div[2]/input");
+                _pass.SendKeys(Pass);
+                Thread.Sleep(1000);
+                var _DangNhap = driver.FindElementByXPath("//*[@id=\"app\"]/div/div/div/form/div[3]/button");
+                _DangNhap.Click();
+            }
+            catch
+            {
+
+            }
+        }
+
+        void DangNhapFaceBook(string ID, string Pass, string FA, ChromeDriver driver)
+        {
+            driver.Url = "http://facebook.com";
+            driver.Navigate();
+            try
+            {
+                var email = driver.FindElementByCssSelector("#email");
+                email.SendKeys(ID);
+
+                var matkhau = driver.FindElementByCssSelector("#pass");
+                matkhau.SendKeys(Pass);
+
+                var dangnhap = driver.FindElementByXPath("//*[@id=\"u_0_b\"]");
+                matkhau.SendKeys(OpenQA.Selenium.Keys.Return);
+
+                //Kiem tra 2FA
+
+
+
+            }
+            catch
+            {
+
+            }
+
+
+        }
+    }
+}
