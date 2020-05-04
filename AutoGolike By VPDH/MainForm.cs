2ec263e98e0f261a9aacd4d17b172e3e7b934eec
LeMinhDong <leminhdong90@gmail.com>
2020-05-04 20:22:31 +0700

    add design

---
+++ AutoGolike By VPDH/MainForm.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AutoGolike_By_VPDH
+{
+    public partial class MainForm : Form
+    {
+        GolikeForm glf = new GolikeForm();
+        FacebookForm fbf = new FacebookForm();
+        SettingsForm stf = new SettingsForm();
+        public MainForm()
+        {
+            InitializeComponent();
+            MainLoadForm();
+        }
+
+        private void MainLoadForm()
+        {
+            IsMdiContainer = true;
+            addForm(glf);
+        }
+        private void addForm(Form f)
+        {
+            f.TopLevel = false;
+            f.AutoScroll = true;
+            f.Dock = DockStyle.Fill;
+            f.FormBorderStyle = FormBorderStyle.None;
+            this.MainView.Controls.Clear();
+            this.MainView.Controls.Add(f);
+            
+            f.Show();
+        }
+        private void BtnMenuGolike_Click(object sender, EventArgs e)
+        {
+            addForm(glf);
+        }
+
+        private void BtnMenuFacebook_Click(object sender, EventArgs e)
+        {
+            FacebookForm fbf = new FacebookForm();
+            addForm(fbf);
+            
+
+        }
+
+        private void BtnMenuSettings_Click(object sender, EventArgs e)
+        {
+            addForm(stf);
+        }
+    }
+}
--- AutoGolike By VPDH/Program.cs
+++ AutoGolike By VPDH/Program.cs
@@ -16,7 +16,7 @@ namespace AutoGolike_By_VPDH
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
